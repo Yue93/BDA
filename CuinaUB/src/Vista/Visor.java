5c0377fdb0a153e0852419f96b1635a87d798989
Yue93 <yue101@gmail.com>
2015-09-26 00:18:04 +0200

    Clases básicas

--- CuinaUB/src/Vista/Visor.java
+++ CuinaUB/src/Vista/Visor.java
@@ -1,6 +1,9 @@
 package Vista;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import Modelo.Usuario;
 
 public class Visor {
 	
@@ -19,6 +22,7 @@ public class Visor {
 	
 	private static String[] opcionsMenuElemento={"Receta","Tipo_Plato","Tipo_Comida","Chef","Ingredientes","Salir"};
 	
+	private ArrayList<Usuario> usuarios=new ArrayList();
 	
 	public static void main(String[] args){
 		nerrors=0;
